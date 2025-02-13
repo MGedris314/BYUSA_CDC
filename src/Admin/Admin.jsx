7573d91cee9d21009e4743c891bdd490b2159598
Matthew Gedris <mdgedris@byu.edu>
2025-02-12 21:14:19 -0700

    Admin JSX

--- src/Admin/Admin.jsx
+++ src/Admin/Admin.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import './Admin.css';
 
-export default function App(){
-    return <div classNameName='body bg-dark text-light'>
-        
+export default function Admin(){
+    return <div className='body bg-dark text-light'>
+<div class="Fields">
+    <label for="username">Username</label>
+        <input type="text" id="username" name="username"></input>
+    <label for="password">Password</label>
+        <input type="text" id="password" name="password"></input>
+    </div>
+    <div class="buttons">
+    <button>
+        Log in
+    </button>
+    <button>
+        Register
+    </button>
+    <a href="index.html">
+        <button>
+            Back
+        </button>
+    </a>
+    </div>
     </div>
 }
--- src/app.jsx
+++ src/app.jsx
@@ -2,36 +2,36 @@ import React from "react";
 import './Home/Home.css';
 
 export default function App(){
-    return <div classNameName='body bg-dark text-light'>
+    return <div classNameNameName='body bg-dark text-light'>
     <title>BYU CSCD</title>
     <header>
         <h1>
             BYU Country swing dance club home page
         </h1>
     </header>
-    <nav className="button_grid">
-    <a href="Calandar.html" className="calandar">
+    <nav classNameName="button_grid">
+    <a href="Calandar.html" classNameName="calandar">
         <button>
             Calandar view
         </button>
     </a>
-    <a className="waver">
+    <a classNameName="waver">
         <button>
             Link to waver
         </button>
 
     </a>
-    <a href="https://clubs.byu.edu/link/Clubs/CSD" target="_blank" className="club_link">
+    <a href="https://clubs.byu.edu/link/Clubs/CSD" target="_blank" classNameName="club_link">
         <button>
             Join the club
         </button>
     </a>
-    <a href="Admin_login.html" className="log_in">
+    <a href="Admin_login.html" classNameName="log_in">
         <button>
             Instructor Log in
         </button>
     </a>
-    <a className="social">
+    <a classNameName="social">
         <button>
             Instagram link
         </button>
@@ -39,7 +39,7 @@ export default function App(){
     </a>
     </nav>
     <footer>
-    <a href="https://github.com/MGedris314/BYUSA_CDC" target="_blank" className="link">
+    <a href="https://github.com/MGedris314/BYUSA_CDC" target="_blank" classNameName="link">
         <p>
             github repository
         </p>
