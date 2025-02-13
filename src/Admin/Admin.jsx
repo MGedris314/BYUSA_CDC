383f7b5e8310461d11f1aa4975ee848aca73d5fc
Matthew Gedris <mdgedris@byu.edu>
2025-02-12 20:05:05 -0700

    Continuing JSX setup

--- src/Admin/Admin.jsx
+++ src/Admin/Admin.jsx
@@ -1,1 +1,8 @@
 import React from "react";
+import './Admin.css';
+
+export default function App(){
+    return <div classNameName='body bg-dark text-light'>
+        
+    </div>
+}
--- src/Calandar/Calandar.jsx
+++ src/Calandar/Calandar.jsx
@@ -1,1 +1,8 @@
 import React from "react";
+import './Calandar.css';
+
+export default function App(){
+    return <div classNameName='body bg-dark text-light'>
+        
+    </div>
+}
