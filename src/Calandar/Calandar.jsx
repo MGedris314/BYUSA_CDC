409766297079008c79d6943d63b1083782c7c52f
Matthew Gedris <mdgedris@byu.edu>
2025-02-12 21:16:08 -0700

    Calandar JSX

--- src/Calandar/Calandar.jsx
+++ src/Calandar/Calandar.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import './Calandar.css';
 
-export default function App(){
+export default function Calandar(){
     return <div classNameName='body bg-dark text-light'>
-        
+        <div class="img_holder">
+        <img src="Blank.jpg" alt="Put calandar here"></img>
+    </div>
+    <div>
+        <p>
+            Websocket updates regarding club anouncements and who's teaching that week.
+        </p>
+    </div>
+    <a href="index.html">
+    <button>
+        Return to home page.
+    </button>
+    <p>
+        If they are loged in as an admin, they will be able to edit the calandar here.
+    </p>
+</a>
     </div>
 }
