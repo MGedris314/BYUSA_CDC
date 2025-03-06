b0f23dd05ad99ec89cde09225ee8cda64038d7b1
Matthew Gedris <mdgedris@byu.edu>
2025-03-06 11:17:04 -0700

    Begining services.

---
+++ src/service.js
@@ -0,0 +1,30 @@
+// Okay In this file (until I learn otherwise) I'm going to be dropping all of my JS code to make this thing run.
+
+//  To start I'll be taking the script from login in class that creates a hashed password, we'll work on storing it later.
+
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const passwords = {};
+
+app.post('/register', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  passwords[req.body.user] = hashedPassword;
+  res.send({ user: req.body.user });
+});
+
+app.put('/login', async (req, res) => {
+  const hashedPassword = passwords[req.body.user];
+  if (hashedPassword && (await bcrypt.compare(req.body.password, hashedPassword))) {
+    res.send({ user: req.body.user });
+  } else {
+    res.send(401, { msg: 'invalid user or password' });
+  }
+});
+
+
+//  Static middle ware syntax is as follows:
+//  app.use(express.static('name of directory to be used aka root.'))
