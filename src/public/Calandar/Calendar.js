8f2c6f4c9a9f7b0a2e58616487741f217a87cc4a
Matthew Gedris <mdgedris@byu.edu>
2025-04-07 20:32:35 -0600

    Trimed websoket from simon.

--- src/public/Calandar/Calendar.js
+++ src/public/Calandar/Calendar.js
@@ -8,13 +8,10 @@ const web_events = {
 };
 
 class Event_message{
-    constructor(from, type, value){
+    constructor(from){
         this.from=from
-        this.type=type
-        this.value=value
     }
 }
-
 export function edditor(){
     if (admin_loggedin==true){
         alert("I'm doing things")
@@ -28,6 +25,56 @@ export function edditor(){
     modal.style.display = "block";
 }
 
+class CallEventNotifier {
+    events = [];
+    handlers = [];
+  
+    constructor() {
+      let port = window.location.port;
+      const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+      this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+      this.socket.onopen = (event) => {
+        this.receiveEvent(new Event_message('Simon', GameEvent.System, { msg: 'connected' }));
+      };
+      this.socket.onclose = (event) => {
+        this.receiveEvent(new Event_message('Simon', GameEvent.System, { msg: 'disconnected' }));
+      };
+      this.socket.onmessage = async (msg) => {
+        try {
+          const event = JSON.parse(await msg.data.text());
+          this.receiveEvent(event);
+        } catch {}
+      };
+    }
+  
+    broadcastEvent(from) {
+      const event = new Event_message(from);
+      this.socket.send(JSON.stringify(event));
+    }
+  
+    addHandler(handler) {
+      this.handlers.push(handler);
+    }
+  
+    removeHandler(handler) {
+      this.handlers.filter((h) => h !== handler);
+    }
+  
+    receiveEvent(event) {
+      this.events.push(event);
+  
+      this.events.forEach((e) => {
+        this.handlers.forEach((handler) => {
+          handler(e);
+        });
+      });
+    }
+  }
+
+const CallNotifer=new CallEventNotifier
+
 export function close(){
     modal.style.display="none"
 }
+
+export {CallNotifer, web_events}
